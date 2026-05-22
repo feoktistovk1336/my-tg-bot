860769e23795da3c6ffc6b9599052bdf6b386bd0
feoktistovk1336 <feoktistovk1336@gmail.com>
2026-05-22 23:10:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+TIMEZONE = "Europe/Moscow"
 # ─────────────────────────────────────────────────────────────────────────────
  
 logging.basicConfig(level=logging.INFO, format="%(asctime)s | %(levelname)s | %(message)s")
