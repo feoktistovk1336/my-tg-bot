7e32d714b47d88f9e98e1c51b02e46dede152107
feoktistovk1336 <feoktistovk1336@gmail.com>
2026-05-22 22:48:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,15 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import (
-    ReplyKeyboardMarkup, KeyboardButton,
-    InlineKeyboardMarkup, InlineKeyboardButton,
-    ReplyKeyboardRemove, InputMediaPhoto,
-    InputMediaVideo, InputMediaAudio
+    Message,
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    ReplyKeyboardRemove,
+    InputMediaPhoto,
+    InputMediaVideo,
+    InputMediaAudio
 )
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.date import DateTrigger
